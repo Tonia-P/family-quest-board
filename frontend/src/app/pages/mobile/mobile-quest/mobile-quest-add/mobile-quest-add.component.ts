47c67e4b262d7f2a3a1dcb9ccce797c980c7c6a3
Nikola <maragkaksimike@gmail.com>
2024-01-15 23:56:40 +0200

    changes to create quest

--- frontend/src/app/pages/mobile/mobile-quest/mobile-quest-add/mobile-quest-add.component.ts
+++ frontend/src/app/pages/mobile/mobile-quest/mobile-quest-add/mobile-quest-add.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SocketsService } from 'src/app/global/services/sockets/sockets.service';
+import { TasksService } from 'src/app/global/services/tasks/tasks.service';
+import { TaskModel } from 'src/app/global/models/tasks/task.model';
 
 @Component({
   selector: 'app-mobile-quest-add',
@@ -9,22 +12,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class MobileQuestAddComponent {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private tasksService: TasksService,
+    private socketService: SocketsService) {
      this.form = this.fb.group({
        type: ['Daily', Validators.required],
        title: ['', Validators.required],
        description: ['', Validators.required],
+       deadline: ['', Validators.required],
        difficulty: ['', Validators.required],
-       reward: ['', Validators.required]
+       reward: ['', Validators.required],
      });
+
   }
  
   onSubmit() {
      if (this.form.valid) {
-       console.log('Form is valid:', this.form.value);
+      const data = new TaskModel(this.form);
+      data.participants = [];
+
+      this.createTask(data);
+      console.log('Form is valid:', this.form.value);
      } else {
        console.log('Form is invalid:', this.form.errors);
      }
+  }
+
+  private createTask(task: TaskModel): void {
+    this.tasksService.create(task).subscribe((result) => {
+      console.log(result);
+    });
   }
  
 }
