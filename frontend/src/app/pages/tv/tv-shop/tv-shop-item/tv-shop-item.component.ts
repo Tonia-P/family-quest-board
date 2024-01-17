31a9e76fce262a1ce5c1302d5fd3b13e4ccd62f2
Nikola <maragkaksimike@gmail.com>
2024-01-18 01:38:50 +0200

    Responsive items

--- frontend/src/app/pages/tv/tv-shop/tv-shop-item/tv-shop-item.component.ts
+++ frontend/src/app/pages/tv/tv-shop/tv-shop-item/tv-shop-item.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { SocketsService } from 'src/app/global/services/sockets/sockets.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Item } from 'src/app/pages/shared/interfaces/item';
+
+import { ItemsService } from 'src/app/global/services/item-shop/item.service';
 
 @Component({
   selector: 'app-tv-shop-item',
@@ -23,13 +26,32 @@ export class TvShopItemComponent implements OnInit {
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
+    private socketService: SocketsService,
+    private itemService: ItemsService
   ) { }
   
 
   ngOnInit(): void {
+    this.socketService.subscribe("Item_Sold", (data: any) => {
+      
+    });
+    this.socketService.subscribe("Mobile_Selected", (data: any) => {
+      this.router.navigate(['/tv/questbook'], { relativeTo: this.activatedRoute});
+    });
   }
 
-  onSelect(): void{
-    this.selectedFlag.emit(this.item)
+  onClick(): void{
+    const data = {
+      event: "Item_Selected",
+      message: this.item._id
+    };
+    this.pingOtherDevicesForTask(data);
+  }
+
+  private pingOtherDevicesForTask(data: any): void {
+    console.log(data);
+    this.itemService.pingOtherDevicesForTask(data).subscribe((result) => {
+      console.log(result);
+    });
   }
 }
