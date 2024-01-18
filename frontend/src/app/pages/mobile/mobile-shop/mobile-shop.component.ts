c0ec4f7455cce055b6363942814535e20f864b9f
Nikola <maragkaksimike@gmail.com>
2024-01-18 18:45:14 +0200

    shop + items done for mobile

--- frontend/src/app/pages/mobile/mobile-shop/mobile-shop.component.ts
+++ frontend/src/app/pages/mobile/mobile-shop/mobile-shop.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
-import { Item } from '../../shared/interfaces/item';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SocketsService } from 'src/app/global/services/sockets/sockets.service';
+import { TasksService } from 'src/app/global/services/tasks/tasks.service';
+import { ShopsService } from 'src/app/global/services/item-shop/shop.service';
+import { Shop } from 'src/app/pages/shared/interfaces/shop';
+import { Item } from 'src/app/pages/shared/interfaces/item';
 
 @Component({
   selector: 'app-mobile-shop',
@@ -29,5 +34,27 @@ export class MobileShopComponent {
       price: 200,
       selected: false
     
+  }
+
+  @Input() id: string | null = null;
+  @Input() itemid: string | null = null;
+
+  constructor(private activatedRoute: ActivatedRoute, private tasksService: TasksService,
+    private socketService: SocketsService, private shopsService: ShopsService) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.id = params.get('id');
+    });
+    const shopId = this.id as string;
+    this.getAllItemsOfShop(shopId);
+  }
+
+  private getAllItemsOfShop(shopId: string): void {
+    this.shopsService.getAllItems(shopId).subscribe((result) => {
+      console.log(result);
+      this.shopItems = result;
+
+    });
   }
 }
